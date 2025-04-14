c9fabe8f7db69e7264ba4a4c872c0353a7ff959b
swmalm <148342347+swmalm@users.noreply.github.com>
2025-04-14 19:08:52 +0200

    Changed to sed instead of grep -oP to stop lspci error -2

--- start.sh
+++ start.sh
@@ -446,7 +446,7 @@ while true; do
 			read -p "Do you want to check for system compatibility and install the nvidia driver? (y/n) > " -n 1 -r
 			printf "\n"
 			if [[ $REPLY =~ ^[Yy]$ ]]; then
-				gpu_model=$(lspci -k | grep VGA | grep NVIDIA | grep -oP '\bCorporation\b\s*\K[A-Za-z]{2}')
+				gpu_model=$(lspci -k | grep VGA | grep NVIDIA | sed -n 's/.*Corporation\s*\([A-Za-z][A-Za-z]\).*/\1/p')
 				if [[ "$gpu_model" =~ (TU|GA|AD)$ ]]; then
 					cpu_model=$(cat /proc/cpuinfo | grep "model name" | head -n 1 | cut -d ':' -f 2 | sed -E 's/.*i[3579]-([0-9]{4}).*/\1/' | cut -c1-2)
 					kernel=$(pacman -Q | grep -E '^linux(| |-lts)[^-headers]' | cut -d ' ' -f 1)
